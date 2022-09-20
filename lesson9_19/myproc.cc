81e80812828896af744fd2e04a7273621fc82cae
15978104402 <guhui4402@foxmail.com>
2022-09-20 22:40:01 +0800

    阻塞式等待

--- lesson9_19/myproc.cc
+++ lesson9_19/myproc.cc
@@ -1,2 +1,30 @@
 #include<iostream>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<stdio.h>
 
+using namespace std;
+
+int main()
+{
+    pid_t id = fork();
+    if (id == 0)
+    {
+        int cnt = 5;
+        while (cnt)
+        {
+            printf("I am child process:%d\n", cnt--);
+            sleep(1);
+        }
+    }
+    else 
+    {
+        int status = 0;
+        pid_t res = waitpid(-1, &status, 0); // 阻塞式等待
+        if (res > 0)
+        {
+            printf("等待子进程成功，退出码:%d\n", WEXITSTATUS(status));
+        }
+    }
+}
