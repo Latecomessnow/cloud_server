5e26683325de4e994485677e34922c4b7deb9c1e
15978104402 <guhui4402@foxmail.com>
2022-10-09 21:26:42 +0800

    day1

---
+++ day1/test1.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> ret;
+    int n = 0;
+    long long sum = 0;
+    for (int i = 0; i < (3 * n); i++)
+    {
+        int val = 0;
+        cin >> val;
+        ret.push_back(val);
+    }
+    sort(ret.begin(), ret.end());
+    int count = n;
+    while (count--)
+    {
+        sum += ret[n];
+        n += 2;
+    }
+    cout << sum << endl;
+    return 0;
+}
