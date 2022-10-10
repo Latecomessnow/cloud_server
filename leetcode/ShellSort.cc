c0f8f5d82cfdbef2470f91d5112171221e0627d2
15978104402 <guhui4402@foxmail.com>
2022-10-10 23:55:11 +0800

    希尔排序

---
+++ leetcode/ShellSort.cc
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+					break;
+			}
+			a[end + gap] = tmp;
+		}
+	}
+	//int gap = 3;
+	//for (int j = 0; j < gap; j++)
+	//{
+	//	for (int i = j; i < n - gap; i += gap)
+	//	{
+	//		int end = i;
+	//		int tmp = a[end + gap];
+	//		while (end >= 0)
+	//		{
+	//			if (tmp < a[end])
+	//			{
+	//				a[end + gap] = a[end];
+	//				end -= gap;
+	//			}
+	//			else
+	//				break;
+	//		}
+	//		a[end + gap] = tmp;
+	//	}
+	//}
+}
+
+int main()
+{
+	int arr[] = { 19, 2, 3, 45, 3, 2, 6, 0, 4 };
+	int a[] = { 9, 1, 2, 5, 7, 4, 8, 6, 3, 5 };
+	ShellSort(a, sizeof(a) / sizeof(int));
+	for (int i = 0; i < sizeof(a) / sizeof(int); i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	ShellSort(arr, sizeof(arr) / sizeof(int));
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
