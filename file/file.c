c5b1ec3215733d5cfaf0d5a67f4ec1ec678762fb
15978104402 <guhui4402@foxmail.com>
2022-10-12 21:07:26 +0800

    文件操作

--- file/file.c
+++ file/file.c
@@ -3,16 +3,20 @@
 
 int main()
 {
-    FILE* pf = fopen("bite", "w+");
-    char arr[] = "linux so easy\n";
-    fwrite(arr, strlen(arr), 1, pf);
-    char str[60];
-    fread(str, strlen(arr), 1, pf);
-    printf("%s\n", str);
-    fclose(pf);
+    // 使用同一个文件指针操作文件需要注意指针的位置
+    // 需要用读写的方式打开，否则读的时候会出现问题
+    FILE* fp = fopen("bite", "w+");
+    char str[] = "linux so easy, hhh\n";
+    fwrite(str, strlen(str), 1, fp);
+    // 此时文件指针已指到文件为，需要fseek一下
+    // 偏移量是从第三个参数开始算的，正往后偏移，负往前偏移
+    fseek(fp, 0, SEEK_SET);
+    char buffer[60];
+    fread(buffer, strlen(str), 1, fp);
+    printf("%s\n", buffer);
+    fclose(fp);
     return 0;
 }
-
 //#include <stdio.h>
 //#include <string.h>
  
