66965d214f738e1a9375bc7680d83934352a9afa
15978104402 <guhui4402@foxmail.com>
2022-11-11 22:48:16 +0800

    vscode

---
+++ vscode/procIpc/Test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<unordered_map>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    std::cout << "hello world" << std::endl;
+    std::cout << "hello world" << std::endl;
+    std::cout << "hello world" << std::endl;
+    std::cout << "hello world" << std::endl;
+    std::cout << "hello world" << std::endl;
+    std::cout << "hello world" << std::endl;
+    std::cout << "hello world" << std::endl;
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
