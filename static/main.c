4e4a46c68df824c6f18b9e4954330cd961c82775
15978104402 <guhui4402@foxmail.com>
2023-01-04 23:26:11 +0800

    静态库

---
+++ static/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<add.h>
+#include<sub.h>
+
+int main()
+{
+    int x = 20;
+    int y = 10;
+    printf("%d + %d = %d\n", x, y, x + y);
+    printf("%d - %d = %d\n", x, y, x - y);
+    return 0;
+}
