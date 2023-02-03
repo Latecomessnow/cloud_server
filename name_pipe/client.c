c486a17189dfb85fbdb5c80c08ab285b300a98f6
15978104402 <guhui4402@foxmail.com>
2023-02-04 01:14:22 +0800

    name_pipe

--- name_pipe/client.c
+++ name_pipe/client.c
@@ -2,6 +2,41 @@
 
 int main()
 {
-
+    // 服务端已创建命名管道，客户端只需打开即可
+    // 因为客户端需要先从标准输出读取数据，再向命名管道中进行写入，所以打开文件时需要读写权限 
+    int fd = open(FILE_NAME, O_WRONLY);
+    if (fd < 0)
+    {
+        perror("open");
+        exit(1);
+    }
+    char msg[128];
+    while (1)
+    {
+        // 每次读取前清空message
+        msg[0] = '\0';
+        printf("Please send message:");
+        fflush(stdout);
+        // 从标准输入读取数据
+        ssize_t s = read(0, msg, sizeof(msg) - 1);
+        if (s > 0)
+        {
+            msg[s - 1] = '\0';
+            // 将msg写入管道中
+            write(fd, msg, strlen(msg));
+        }
+        else if (s == 0)
+        {
+            printf("file end\n");
+            break;
+        }
+        else
+        {
+            perror("write");
+            exit(1);
+        }
+    }
+    // 关闭文件
+    close(fd);
     return 0;
 }
--- name_pipe/server.c
+++ name_pipe/server.c
@@ -1,1 +1,53 @@
-3
+#include"comm.h"
+
+int main()
+{
+    umask(0);
+    // 服务端创建好命名管道以供客户端进行写入，然后服务端在进行读取
+    if (mkfifo(FILE_NAME, 0666) < 0)
+    {
+        perror("mkfifo");
+        exit(1);
+    }
+
+    // 进程间通信的本质就是让不同的进程看到同一份资源
+    // 不同进程子需要打开同一个命名管道即可
+    int fd = open(FILE_NAME, O_RDONLY);
+    if (fd < 0)
+    {
+        perror("open");
+        exit(2);
+    }
+    char buffer[128];
+    while (1)
+    {
+        // 每次读之前需要把buffer清空
+        buffer[0] = '\0';
+        // sizeof减1，因为系统级别的接口不关心C语言以\0结尾的规则
+        ssize_t s = read(fd, buffer, sizeof(buffer) - 1);
+        if (s > 0)
+        {
+            // 手动设置\0，便于输出
+            buffer[s] = '\0';
+            // 输出客户端发送过来的信息
+            printf("client send:%s\n", buffer);
+        }
+
+        // 正常结束读到0
+        else if(s == 0)
+        {
+            printf("client quit!\n");
+            break;
+        }
+        else
+        {
+            perror("read");
+            exit(3);
+        }
+
+        // 通信完毕，关闭命名管道
+    }
+
+    close(fd);
+    return 0;
+}
