d67c15a054a33e06a76a04fc8e6cc6ca137c09a5
15978104402 <guhui4402@foxmail.com>
2023-02-12 23:38:05 +0800

    共享内存

---
+++ systemV/comm.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/ipc.h>
+
+#define PATHNAME "/home/guhui/cloud_server/systemV"
+
+// 整数标识符
+#define PROJ_ID 0X666
+// 共享内存的大小
+#define SIZE 4096
