3836d5101db68d97910ac11a931e8fcec426ed5b
15978104402 <guhui4402@foxmail.com>
2023-02-26 23:36:45 +0800

    signal

--- signal/signal.cc
+++ signal/signal.cc
@@ -1,26 +1,49 @@
-#include<iostream>
-#include<signal.h>
-#include<sys/types.h>
-#include<unistd.h>
+//#include<iostream>
+//#include<signal.h>
+//#include<sys/types.h>
+//#include<unistd.h>
+//
+//using namespace std;
+//
+//void catch_signal(int sigalnum)
+//{
+//    cout << "进程捕捉到了一个信号，正在处理中：" << sigalnum << " pid:" << getpid() << endl;
+//}
+//
+//int main()
+//{
+//    // 先注册方法，如果出现二号进程就进行回调
+//    // 提前设置了对特定信号的处理方法，如果没有SIGINT信号，那么catch_signal就不会被调用
+//    signal(SIGINT, catch_signal);
+//
+//    while (true)
+//    {
+//        cout << "我是一个进程, 我正在运行... pid：" << getpid() << endl; 
+//        sleep(1);
+//    }
+//
+//    return 0;
+//}
+
+
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>
 
 using namespace std;
 
-void catch_signal(int sigalnum)
+void handler(int sig)
 {
-    cout << "进程捕捉到了一个信号，正在处理中：" << sigalnum << " pid:" << getpid() << endl;
+    cout << endl << "The process recevivd a signal:" << sig << endl;
 }
 
 int main()
 {
-    // 先注册方法，如果出现二号进程就进行回调
-    // 提前设置了对特定信号的处理方法，如果没有SIGINT信号，那么catch_signal就不会被调用
-    signal(SIGINT, catch_signal);
-
-    while (true)
+    signal(SIGINT, handler);
+    while (1)
     {
-        cout << "我是一个进程, 我正在运行... pid：" << getpid() << endl; 
+        cout << "hello horld" << endl;
         sleep(1);
     }
-
     return 0;
 }
