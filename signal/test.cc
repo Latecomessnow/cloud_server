432f6ef4f89191e1c973b3c85900e7faba7d8ec2
15978104402 <guhui4402@foxmail.com>
2023-03-01 17:19:27 +0800

    signal

--- signal/test.cc
+++ signal/test.cc
@@ -34,29 +34,32 @@ using namespace std;
 
 int main()
 {
-    // fork创建子进程成功后,父进程返回子进程的pid,子进程返回0
-    // fork函数存在两个返回值
-    pid_t id = fork();
-    if (id == 0)
-    {
-        // child
-        cout << "I'm child, running..." << endl;
-        cout << "pid:" << getpid() << " "
-             << "ppid:" << getppid() << endl;
-        int *p = nullptr;
-        *p = 100;
-        // 退出子进程,观察core dump标志位
-        //exit(0);
-    }
-    cout << "I'm father, running..." << endl;
-    cout << "pid:" << getpid() << " "
-         << "ppid:" << getppid() << endl;
-    int status = 0;
-    // 父进程返回的pid正是紫禁城的pid,所以可以直接在waitpid等待子进程
-    pid_t change = waitpid(id, &status, 0);
-    // 退出码, core dump标志位, 退出信号
-    cout << "ExitCode:" << ((status >> 8) & 0xff) << endl
-         << "core dump:" << ((status >> 7) & 1) << endl
-         << "ExitSignal:" << (status & 0x7f) << endl;
-    return 0;
+     // fork创建子进程成功后,父进程返回子进程的pid,子进程返回0
+     // fork函数存在两个返回值
+     pid_t id = fork();
+     if (id == 0)
+     {
+          // child
+          cout << "I'm child, running..." << endl;
+          cout << "pid:" << getpid() << " "
+               << "ppid:" << getppid() << endl;
+          int *p = nullptr;
+          *p = 100;
+          // 退出子进程,观察core dump标志位
+          //exit(0);
+     }
+     else
+     {
+          cout << "I'm father, running..." << endl;
+          cout << "pid:" << getpid() << " "
+               << "ppid:" << getppid() << endl;
+          int status = 0;
+          // 父进程返回的pid正是紫禁城的pid,所以可以直接在waitpid等待子进程
+          pid_t change = waitpid(id, &status, 0);
+          // 退出码, core dump标志位, 退出信号
+          cout << "ExitCode:" << ((status >> 8) & 0xff) << endl
+               << "core dump:" << ((status >> 7) & 1) << endl
+               << "ExitSignal:" << (status & 0x7f) << endl;
+     }
+     return 0;
 }
