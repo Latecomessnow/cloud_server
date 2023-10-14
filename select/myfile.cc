5a4aab64c79d085e48c33b0e76149c4e9a4c48e7
Latecomessnow <hui.yumhappy@gmail.com>
2023-10-14 20:58:58 +0800

    select

--- select/myfile.cc
+++ select/myfile.cc
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <unistd.h>
+#include <fcntl.h>
 
 using namespace std;
 
+bool SetNonBlock(int fd)
+{
+    // 1. 获取零号文件描述符的读写标志位
+    int fl = fcntl(fd, F_GETFL);
+    if (fl < 0) return false;
+    // 2. 将零号文件描述符设置成非阻塞
+    fcntl(fd, F_SETFL, fl | O_NONBLOCK); // 注意需要传入读写标志位
+    return true;
+}
+
 int main()
 {
+    // 将标准输入设置成非阻塞
+    SetNonBlock(0); // 只需要设置一次后续都是非阻塞的了
     char buffer[1024];
     while (true)
     {
@@ -19,6 +32,7 @@ int main()
         {
             // TODO 
             cout << "read \"error\"" << endl;
+            sleep(1);
         }
     }
     return 0;
